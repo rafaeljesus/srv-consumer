8d33d30abc03ccd45b90707d91f771bd5a7c9037
ikechukwu edwin <eo@hellofresh.com>
2018-05-27 09:57:30 +0200

    Add some tiny test helper functions to make code neater.

---
+++ test_helper/assert.go
@@ -0,0 +1,23 @@
+package test_helper
+
+import (
+	"testing"
+)
+
+// ok fails the test if an err is not nil.
+func Ok(tb testing.TB, err error) {
+	if err != nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
+		tb.FailNow()
+	}
+}
+
+// assert fails the test if the condition is false.
+func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+	if !condition {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
+		tb.FailNow()
+	}
+}
